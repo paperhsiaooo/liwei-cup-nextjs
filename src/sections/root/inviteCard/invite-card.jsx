136c15ffd4514099472c948584c5dc2bd7a2ce34
paperhsiaooo <paper.hsiaooo@gmail.com>
2025-08-09 12:00:13 +0800

    feat: 調整 progress component

--- src/sections/root/inviteCard/invite-card.jsx
+++ src/sections/root/inviteCard/invite-card.jsx
@@ -4,15 +4,18 @@ import ProgressStep from '@/components/progress-step/progress-step'
 import useUserContext from '@/store/user-context'
 
 import { InviteForm, UserForm } from '.'
+import useProgressContext from './store/progress-context'
 
 function InviteCard() {
   const user = useUserContext(state => state.user)
+  const currentStep = useProgressContext(state => state.currentStep)
+  const stepData = useProgressContext(state => state.data)
 
   return (
     <section className="root bg-green-primary">
       <div className="wrapper">
         <div className="w-full max-w-[940px] mx-auto py-4">
-          <ProgressStep />
+          <ProgressStep currentStep={currentStep} stepData={stepData} />
         </div>
         {user.isLogin ? <UserForm user={user} /> : <InviteForm />}
       </div>
