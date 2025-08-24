f8f5c89eab595d6919b7adf21779b686b41e6e80
paperhsiaooo <paper.hsiaooo@gmail.com>
2025-08-24 17:00:34 +0800

    feat: 調整 countDown components prop

--- src/sections/root/main/countDownTimer.jsx
+++ src/sections/root/main/countDownTimer.jsx
@@ -2,8 +2,9 @@
 
 import { memo } from 'react'
 import { useTimer } from 'react-timer-hook'
+import { twMerge } from 'tailwind-merge'
 
-function CountDownTimer() {
+function CountDownTimer({ type = 'primary' | 'secondary' }) {
   const { days, hours, minutes, seconds } = useTimer({
     expiryTimestamp: new Date(2025, 11, 15, 12, 0, 0),
   })
@@ -11,22 +12,82 @@ function CountDownTimer() {
   return (
     <div className="relative z-10">
       <p className="text-center font-anton font-normal text-4xl text-white flex justify-center items-end flex-row gap-x-3">
-        <span suppressHydrationWarning={true} className="text-[30px]">
+        <span
+          suppressHydrationWarning={true}
+          className={twMerge(
+            'text-[30px]',
+            type === 'primary' && 'text-white',
+            type === 'secondary' && 'text-blue-primary',
+          )}
+        >
           {days}
         </span>
-        <span className="text-[14px]">DAYS</span>
-        <span suppressHydrationWarning={true} className="text-[30px]">
+        <span
+          className={twMerge(
+            'text-[14px]',
+            type === 'primary' && 'text-white',
+            type === 'secondary' && 'text-green-primary',
+          )}
+        >
+          DAYS
+        </span>
+        <span
+          suppressHydrationWarning={true}
+          className={twMerge(
+            'text-[30px]',
+            type === 'primary' && 'text-white',
+            type === 'secondary' && 'text-blue-primary',
+          )}
+        >
           {hours}
         </span>
-        <span className="text-[14px]">HRS</span>
-        <span suppressHydrationWarning={true} className="text-[30px]">
+        <span
+          className={twMerge(
+            'text-[14px]',
+            type === 'primary' && 'text-white',
+            type === 'secondary' && 'text-green-primary',
+          )}
+        >
+          HRS
+        </span>
+        <span
+          suppressHydrationWarning={true}
+          className={twMerge(
+            'text-[30px]',
+            type === 'primary' && 'text-white',
+            type === 'secondary' && 'text-blue-primary',
+          )}
+        >
           {minutes}
         </span>
-        <span className="text-[14px]">MINS</span>
-        <span suppressHydrationWarning={true} className="text-[30px]">
+        <span
+          className={twMerge(
+            'text-[14px]',
+            type === 'primary' && 'text-white',
+            type === 'secondary' && 'text-green-primary',
+          )}
+        >
+          MINS
+        </span>
+        <span
+          suppressHydrationWarning={true}
+          className={twMerge(
+            'text-[30px]',
+            type === 'primary' && 'text-white',
+            type === 'secondary' && 'text-blue-primary',
+          )}
+        >
           {seconds}
         </span>
-        <span className="text-[14px]">SEC</span>
+        <span
+          className={twMerge(
+            'text-[14px]',
+            type === 'primary' && 'text-white',
+            type === 'secondary' && 'text-green-primary',
+          )}
+        >
+          SEC
+        </span>
       </p>
     </div>
   )
