266994ca707a21184324521af44fbce8b388261b
paperhsiaooo <paper.hsiaooo@gmail.com>
2025-08-29 10:03:46 +0800

    feat: 修正 cache policy

--- src/sections/root/declarations/components/card-container.jsx
+++ src/sections/root/declarations/components/card-container.jsx
@@ -1,59 +1,51 @@
-import { unstable_cache } from 'next/cache'
-
 import { CACHE_KEY } from '@/constants/cache-key'
 import { URL } from '@/constants/url'
 import { VERSION } from '@/constants/version'
 
 import CardList from './card-list'
 
-const getUserBattleDeclarations = unstable_cache(
-  async () => {
-    try {
-      const res = await fetch(
-        `${process.env.BASE_URL}/api/list/getBattleDeclarationList`,
-        {
-          cache: 'no-store',
+const getUserBattleDeclarations = async () => {
+  try {
+    const res = await fetch(
+      `${process.env.BASE_URL}/api/list/getBattleDeclarationList`,
+      {
+        next: {
+          revalidate: 60 * 5,
+          tags: [CACHE_KEY.BATTLE_DECLARATIONS],
         },
-      )
+      },
+    )
 
-      if (!res.ok) {
-        throw new Error('Failed to fetch battle declarations')
-      }
-
-      const data = await res.json()
-      return data.data
-    } catch (error) {
-      console.error('>>> [getBattleDeclarations] error: ', error)
-      return []
+    if (!res.ok) {
+      throw new Error('Failed to fetch battle declarations')
     }
-  },
-  [CACHE_KEY.BATTLE_DECLARATIONS],
-  {
-    tags: [CACHE_KEY.BATTLE_DECLARATIONS],
-    revalidate: 60 * 5,
-  },
-)
 
-const getDeclarationsOptions = unstable_cache(
-  async () => {
-    try {
-      const url = `${URL.BattleListCDN}${VERSION.BattleListCDN}/DeclarationsList.json`
+    const data = await res.json()
+    return data.data
+  } catch (error) {
+    console.error('>>> [getBattleDeclarations] error: ', error)
+    return []
+  }
+}
 
-      const res = await fetch(url, { cache: 'no-store' })
+const getDeclarationsOptions = async () => {
+  try {
+    const url = `${URL.BattleListCDN}${VERSION.BattleListCDN}/DeclarationsList.json`
 
-      const data = await res.json()
-      return data
-    } catch (error) {
-      console.error('>>> [getDeclarationsOptions] error: ', error)
-      return []
-    }
-  },
-  [CACHE_KEY.DECLARATIONS_OPTIONS],
-  {
-    tags: [CACHE_KEY.DECLARATIONS_OPTIONS],
-    revalidate: 60 * 60 * 24,
-  },
-)
+    const res = await fetch(url, {
+      next: {
+        revalidate: 60 * 60 * 24,
+        tags: [CACHE_KEY.DECLARATIONS_OPTIONS],
+      },
+    })
+
+    const data = await res.json()
+    return data
+  } catch (error) {
+    console.error('>>> [getDeclarationsOptions] error: ', error)
+    return []
+  }
+}
 
 async function CardContainer() {
   const battleDeclarations = await getUserBattleDeclarations()
