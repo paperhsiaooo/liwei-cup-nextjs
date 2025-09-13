e245573705b7a67aa8b0b2085a11b3cb09d2f8ad
paperhsiaooo <paper.hsiaooo@gmail.com>
2025-09-13 18:56:11 +0800

    feat: format

--- next.config.mjs
+++ next.config.mjs
@@ -35,7 +35,7 @@ const nextConfig = {
           },
           {
             key: 'Permissions-Policy',
-            value: "camera=(), microphone=(), geolocation=()",
+            value: 'camera=(), microphone=(), geolocation=()',
           },
           {
             key: 'Cross-Origin-Opener-Policy',
