a873f870bbfe5aeb734e9bf0e4774842d3ed31c1
paperhsiaooo <paper.hsiaooo@gmail.com>
2025-10-04 09:58:46 +0800

    feat: 首頁電腦板

--- src/sections/root/memory/memory.jsx
+++ src/sections/root/memory/memory.jsx
@@ -1,7 +1,7 @@
 function Memory() {
   return (
     <div className="bg-blue-primary">
-      <div className="max-[375px] mx-auto w-full h-[150px]"></div>
+      <div className="max-[375px] mx-auto w-full h-[1000px]"></div>
     </div>
   )
 }
