0cd6ee87ebd72ac9723121841c3eaec87b612d14
paperhsiaooo <paper.hsiaooo@gmail.com>
2025-10-05 10:33:47 +0800

    style: 調整 heigh content

--- src/layout/bg-layout.jsx
+++ src/layout/bg-layout.jsx
@@ -1,15 +1,15 @@
 function BgLayout({ children }) {
   return (
-    <div>
-      <div className="pb-1 border-[#FFCC08] border-b-4 mb-1">
+    <div className="flex flex-col h-svh">
+      <div className="pb-1 border-[#FFCC08] border-b-4 mb-1 shrink-0">
         <div className="w-full h-8 [background-image:url('/images/deco-01.png')] bg-repeat bg-size-[auto_32px]" />
       </div>
-      <div className="bg-[#ffcc08]">
+      <div className="bg-[#ffcc08] flex-1">
         <div className="wrapper">
           <div className="py-6">{children}</div>
         </div>
       </div>
-      <div className="pt-1 border-[#FFCC08] border-t-4 mt-1">
+      <div className="pt-1 border-[#FFCC08] border-t-4 mt-1 shrink-0">
         <div className="w-full h-8 [background-image:url('/images/deco-01.png')] bg-repeat bg-size-[auto_32px]" />
       </div>
     </div>
