d523ad7712adafe7bf3aa59e267b0fabb99927e0
paperhsiaooo <paper.hsiaooo@gmail.com>
2025-11-02 18:40:06 +0800

    feat: 建立 music section

---
+++ src/sections/root/music/music.jsx
@@ -0,0 +1,11 @@
+import SpotifyEmbed from '@/components/spotify-embed'
+
+function Music() {
+  return (
+    <div className="max-w-[350px] mx-auto mb-6 px-4 1440:max-w-[660px] 1440:mb-16">
+      <SpotifyEmbed />
+    </div>
+  )
+}
+
+export default Music
